4595bbcd3d0f4aa9dc3b7bcd815babb371aaf33e
Kyle Gracey <kyle@kylegracey.com>
2017-05-03 10:40:30 -0500

    Jade template set up for Add user

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
     // Populate the user table on initial page load
     populateTable();
 
+    // Username link click
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
 });
 
 // Functions =============================================================
@@ -22,14 +25,38 @@ function populateTable() {
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
+          userListData = data;
+          tableContent += '<tr>';
+          tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+          tableContent += '<td>' + this.email + '</td>';
+          tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+          tableContent += '</tr>';
         });
 
         // Inject the whole content string into our existing HTML table
         $('#userList table tbody').html(tableContent);
     });
+};
+
+// Show User info
+function showUserInfo(event){
+
+  // Prevent link from firing
+  event.preventDefault();
+
+  // Retrieve username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // Get index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  // Get our user object
+  var thisUserObject = userListData[arrayPosition];
+
+  // Populate Info box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+
 };
